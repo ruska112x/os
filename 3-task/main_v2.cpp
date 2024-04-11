6effbb54e1d3cfaa7ed6f81c387ab2538aa7620f
Ruslan Karabalin <karabalin112@gmail.com>
2024-04-11 10:58:54 +0600

    feat: devide 2 versions

---
+++ 3-task/main_v2.cpp
@@ -0,0 +1,115 @@
+#include <dirent.h>
+#include <iostream>
+#include <string.h>
+#include <string>
+#include <sys/stat.h>
+#include <unistd.h>
+
+struct cgetcwd_r {
+  int error = 0;
+  std::string path = "";
+  int count_of_symlink = 0;
+};
+
+std::ostream &operator<<(std::ostream &os, cgetcwd_r r) {
+  return os << "Error: " << r.error << "\n"
+            << "Path: " << r.path << "\n"
+            << "Count of symbolic links: " << r.count_of_symlink << "\n";
+}
+
+cgetcwd_r cgetcwd() {
+  // variable for result
+  cgetcwd_r result;
+
+  // stat of current working directory
+  struct stat cwd_stat;
+  if (stat(".", &cwd_stat) == -1) {
+    result.error = -1;
+    return result;
+  }
+
+  // stat of root directory
+  struct stat rd_stat;
+  if (stat("/", &rd_stat) == -1) {
+    result.error = -1;
+    return result;
+  }
+
+  // stat of checking directory
+  struct stat cd_stat;
+  // stat of previous checking direcotry
+  struct stat pcd_stat;
+  // dirstream of checking directory
+  DIR *cd_dir;
+  do {
+    cd_dir = opendir(".");
+    if (cd_dir == NULL) {
+      result.error = -1;
+      return result;
+    }
+    if (stat(".", &cd_stat) == -1) {
+      result.error = -1;
+      return result;
+    }
+    errno = 0;
+    struct dirent *ecd_dirent;
+    while ((ecd_dirent = readdir(cd_dir)) != NULL) {
+      struct stat ecd_stat;
+      if (lstat(ecd_dirent->d_name, &ecd_stat) == -1) {
+        result.error = -1;
+        return result;
+      }
+      if (!S_ISLNK(ecd_stat.st_mode)) {
+        // checking for path
+        if (strcmp(ecd_dirent->d_name, ".") != 0 &&
+            strcmp(ecd_dirent->d_name, "..") != 0) {
+          struct stat path_stat;
+          if (stat(ecd_dirent->d_name, &path_stat) == -1) {
+            result.error = -1;
+            return result;
+          }
+          if (path_stat.st_dev == pcd_stat.st_dev &&
+              path_stat.st_ino == pcd_stat.st_ino) {
+            result.path.insert(0, ecd_dirent->d_name);
+            result.path.insert(0, "/");
+          }
+        }
+      }
+    }
+    pcd_stat = cd_stat;
+    if (closedir(cd_dir) == -1) {
+      result.error = -1;
+      return result;
+    }
+    if (errno != 0) {
+      result.error = -1;
+      return result;
+    }
+    if (chdir("..") == -1) {
+      result.error = -1;
+      return result;
+    }
+  } while (cd_stat.st_dev != rd_stat.st_dev ||
+           cd_stat.st_ino != rd_stat.st_ino);
+  if (errno != 0) {
+    result.error = -1;
+    return result;
+  }
+  return result;
+}
+
+// if (S_ISLNK(ecd_stat.st_mode)) {
+//   if (stat(ecd_dirent->d_name, &ecd_stat) == -1) {
+//     result.error = -1;
+//     return result;
+//   }
+//   if (ecd_stat.st_dev == cwd_stat.st_dev &&
+//       ecd_stat.st_ino == cwd_stat.st_ino) {
+//     ++result.count_of_symlink;
+//   }
+// }
+
+int main() {
+  std::cout << cgetcwd();
+  return 0;
+}
